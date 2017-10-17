5b3752e88c7ab7d0f0c5f77573350c13d7462687
Luiz Fernando Peres <luiz_peres10@hotmail.com>
2017-10-17 00:33:37 -0700

    Info is done

---
+++ mojosmart/mojosmart/Classes/Controllers/InfoViewController.h
@@ -0,0 +1,13 @@
+//
+//  InfoViewController.h
+//  mojosmart
+//
+//  Created by Luiz Peres on 2017-10-17.
+//  Copyright © 2017 Luiz Peres. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InfoViewController : UIViewController
+
+@end
---
+++ mojosmart/mojosmart/Classes/Controllers/InfoViewController.m
@@ -0,0 +1,33 @@
+//
+//  InfoViewController.m
+//  mojosmart
+//
+//  Created by Luiz Peres on 2017-10-17.
+//  Copyright © 2017 Luiz Peres. All rights reserved.
+//
+
+#import "InfoViewController.h"
+
+@interface InfoViewController ()
+
+@end
+
+@implementation InfoViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(tap)];
+    [self.view addGestureRecognizer:tap];
+}
+
+- (void) tap
+{
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+@end
