e45e8efac135c1aaaf7de23c95d5527ad1821b77
Luiz Fernando Peres <luiz_peres10@hotmail.com>
2017-10-16 22:17:47 -0700

    Creating first view for app

--- mojosmart/mojosmart/ViewController.m
+++ mojosmart/mojosmart/ViewController.m
@@ -8,7 +8,9 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UIImagePickerControllerDelegate, UINavigationControllerDelegate>
+
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
 
 @end
 
@@ -16,8 +18,25 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    [self launchCameraAnimated:NO];
 }
 
+- (void) launchCameraAnimated:(BOOL)animated
+{
+    UIImagePickerController *imagePicker = [[UIImagePickerController alloc] init];
+    imagePicker.delegate = self;
+    imagePicker.sourceType =  UIImagePickerControllerSourceTypeCamera;
+    imagePicker.allowsEditing = NO;
+    [self presentViewController:imagePicker animated:animated completion:nil];
+}
+
+- (void) imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<NSString *,id> *)info
+{
+    UIImage *image = [info objectForKey:UIImagePickerControllerOriginalImage];
+    self.imageView.image = image;
+    [picker dismissViewControllerAnimated:YES completion:nil];
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
